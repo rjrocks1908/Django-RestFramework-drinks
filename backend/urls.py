049097d5a1982f60d648e451b0738fcb44d44bb2
Rahul Jain <rj.rahul1908@gmail.com>
2023-11-19 12:43:05 +0530

    CRUD operation in DRF

--- backend/urls.py
+++ backend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('drinks/', views.drink_list)
+    path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_details)
 ]
--- backend/views.py
+++ backend/views.py
@@ -11,7 +11,7 @@ def drink_list(request):
     if request.method == "GET":
         drinks = Drink.objects.all()
         serializer = DrinkSerializer(drinks, many=True)
-        return JsonResponse({"drinks": serializer.data}, safe=False)
+        return Response(serializer.data)
 
     if request.method == "POST":
         serializer = DrinkSerializer(data=request.data)
@@ -19,3 +19,24 @@ def drink_list(request):
             serializer.save()
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def drink_details(request, id):
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = DrinkSerializer(instance=drink)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PUT":
+        serializer = DrinkSerializer(instance=drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
